cca2ca08d7a14eaf5f465125f1129b2af5295741
Hartmut Holzgraefe <hholzgra@php.net>
2006-02-13 13:42:29 +0000

    better handler function and flag support (work in progress)
    git-svn-id: http://svn.php.net/repository/pear/packages/CodeGen_MySQL_Plugin/trunk@207063 c90b9560-bf6c-de11-be94-00142212c4b1

--- MySQL/Plugin/Element/Storage.php
+++ MySQL/Plugin/Element/Storage.php
@@ -43,70 +43,197 @@ require_once "CodeGen/MySQL/Plugin/Element.php";
 class CodeGen_MySQL_Plugin_Element_Storage
   extends CodeGen_MySQL_Plugin_Element
 {
-	/**
-	 * Constructor
-	 */
-	function __construct()
-	{
-	  parent::__construct();
-	}
-	
+    /**
+     * Constructor
+     */
+    function __construct()
+    {
+      parent::__construct();
+    }
+    
+    /**
+     * Storage array for function handlers
+     *
+     * @var array
+     */
+    protected $functions = array();
 
-	/**
-	 * Plugin type specifier is needed for plugin registration
-	 *
-	 * @param  void
-	 * @return string
-	 */
-	function getPluginType() 
-	{
-	  return "MYSQL_STORAGE_ENGINE_PLUGIN";
-	}
-	
-	
-	function getPluginCode()
-	{
+
+    /**
+     * Handler flags
+     *
+     * @var array
+     */
+    protected $haFlags = array();
+
+    /**
+     * Set Hander flag by name
+     *
+     * @param  string  flag name
+     * @param  bool    flag value (default=true)
+     * @return bool    success status
+     */
+    function setHaFlag($name, $value = true)
+    {
+        switch ($name) {
+        case "CAN_CREATE":
+        case "CLOSE_CURSORS_AT_COMMIT":
+        case "ALTER_NOT_SUPPORTED":
+        case "CAN_RECREATE":
+        case "HIDDEN":
+        case "FLUSH_AFTER_RENAME":
+        case "NOT_USER_SELECTABLE":
+            if (isset($this->haFlags["HTON_".$name])) {
+                return PEAR::raiseError("handlerton flag '$name' set twice");
+            }
+            $this->haFlags["HTON_".$name] = $value;
+            return true;
+        default: 
+            return PEAR::raiseError("unknown handlerton flag '$name'");
+        }
+    }
+
+    /**
+     * Set function handler code
+     *
+     * @param  Handler name
+     * @param  Code snippet
+     */
+    function setFunction($name, $code)
+    {
+        if (!$this->isName($name)) {
+            return PEAR::raiseError("'$name' is not a valid handler function");
+        }
+        if (isset($this->functions[$name])) {
+            return PEAR::raiseError("'$name' function declared twice");
+        }
+        $this->functions[$name] = $code;
+
+        return true; 
+    }
+
+
+    /**
+     * Return function name for handler if implemented, else "NULL"
+     *
+     * @param  string  handler name
+     * @return bool    function name if implemented, else "NULL"
+    function funcName($name)
+    {
+        if isset($this->functions[$name]) {
+            return strtolower($this->name)."_".$name;
+        } else {
+            return "NULL";
+        }
+    }
+
+
+    /**
+     * Plugin type specifier is needed for plugin registration
+     *
+     * @param  void
+     * @return string
+     */
+    function getPluginType() 
+    {
+      return "MYSQL_STORAGE_ENGINE_PLUGIN";
+    }
+    
+
+
+    /**
+     * Check for valid handler function names
+     *
+     * @param  string  handler name
+     * @return bool    true for valid handler names, else false
+     */
+    function isName($name)
+    {
+        return in_array($name, array("close_connection",
+                                     "savepoint",
+                                     "savepoint_rollback",
+                                     "savepoint_release",
+                                     "commit",
+                                     "rollback",
+                                     "prepare",
+                                     "recover",
+                                     "commit_by_xid",
+                                     "rollback_by_xid",
+                                     "create_cursor_read_view",
+                                     "set_cursor_read_view",
+                                     "close_cursor_read_view",
+                                     "create_handler",
+                                     "drop_database",
+                                     "panic_call",
+                                     "release_temporary_latches",
+                                     "update_statistics",
+                                     "start_snapshot",
+                                     "flush_logs",
+                                     "show_status",
+                                     "partition_flags",
+                                     "alter_table_flags",
+                                     "alter_tablespace",
+                                     "fill_files_table",
+                                     "binlog_func",
+                                     "binlog_log_query"
+                                     ));
+    }
+
+
+    function getPluginCode()
+    {
       $name    = $this->name;
-	  $lowname = strtolower($name);
-	  $upname  = strtoupper($name);
+      $lowname = strtolower($name);
+      $upname  = strtoupper($name);
 
-	  return parent::getPluginCode()."
+      $code = parent::getPluginCode()."\n";
 
-handlerton {$lowname}_hton= {
-  \"$upname\",
-  SHOW_OPTION_YES,
-  \"{$this->summary}\", 
-  DB_TYPE_CUSTOM,
-  NULL,    /* Initialize */
-  0,       /* slot */
-  0,       /* savepoint size. */
-  NULL,    /* close_connection */
-  NULL,    /* savepoint */
-  NULL,    /* rollback to savepoint */
-  NULL,    /* release savepoint */
-  NULL,    /* commit */
-  NULL,    /* rollback */
-  NULL,    /* prepare */
-  NULL,    /* recover */
-  NULL,    /* commit_by_xid */
-  NULL,    /* rollback_by_xid */
-  NULL,    /* create_cursor_read_view */
-  NULL,    /* set_cursor_read_view */
-  NULL,    /* close_cursor_read_view */
-  {$lowname}_create_handler,    /* Create a new handler */
-  NULL,    /* Drop a database */
-  NULL,    /* Panic call */
-  NULL,    /* Release temporary latches */
-  NULL,    /* Update Statistics */
-  NULL,    /* Start Consistent Snapshot */
-  NULL,    /* Flush logs */
-  NULL,    /* Show status */
-  NULL,    /* Replication Report Sent Binlog */
-  HTON_CAN_RECREATE
-};
+      $code.="handlerton {$lowname}_hton= {\n";
+      $code.="  \"$upname\",\n";
+      $code.="  SHOW_OPTION_YES,\n";
+      $code.="  \"{$this->summary}\",\n"; 
+      $code.="  DB_TYPE_CUSTOM,\n";
+      $code.="  ".$this->funcName("init").", /* Initialize */\n";
+      $code.="  0,       /* slot */\n";
+      $code.="  0,       /* savepoint size. */\n";
+      $code.="  ".$this->funcName("close_connection").", /* close_connection */\n";
+      $code.="  ".$this->funcName("savepoint").", /* savepoint */\n";
+      $code.="  ".$this->funcName("savepoint_rollback").", /* rollback to savepoint */\n";
+      $code.="  ".$this->funcName("savepoint_release").", /* release savepoint */\n";
+      $code.="  ".$this->funcName("commit").", /* commit */\n";
+      $code.="  ".$this->funcName("rollback").", /* rollback */\n";
+      $code.="  ".$this->funcName("prepare").", /* prepare */\n";
+      $code.="  ".$this->funcName("recover").", /* recover */\n";
+      $code.="  ".$this->funcName("commit_by_xid").", /* commit_by_xid */\n";
+      $code.="  ".$this->funcName("rollback_by_xid").", /* rollback_by_xid */\n";
+      $code.="  ".$this->funcName("create_cursor_read_view").", /* create_cursor_read_view */\n";
+      $code.="  ".$this->funcName("set_cursor_read_view").", /* set_cursor_read_view */\n";
+      $code.="  ".$this->funcName("close_cursor_read_view").", /* close_cursor_read_view */\n";
+      $code.="  ".$this->funcName("create_handler").", /* Create a new handler */\n";
+      $code.="  ".$this->funcName("drop_database").", /* Drop a database */\n";
+      $code.="  ".$this->funcName("panic_call").", /* Panic call */\n";
+      $code.="  ".$this->funcName("release_temporary_latches").", /* Release temporary latches */\n";
+      $code.="  ".$this->funcName("update_statistics").", /* Update Statistics */\n";
+      $code.="  ".$this->funcName("start_snapshot").", /* Start Consistent Snapshot */\n";
+      $code.="  ".$this->funcName("flush_logs").", /* Flush logs */\n";
+      $code.="  ".$this->funcName("show_status").", /* Show status */\n";
 
-";
+      $code.="  ".$this->funcName("partition_flags").", /* */\n";
+      $code.="  ".$this->funcName("alter_table_flags").", /* */\n";
+      $code.="  ".$this->funcName("alter_tablespace").", /* */\n";
+      $code.="  ".$this->funcName("fill_files_table").", /* */\n";
 
+      $flags = array();
+      foreach ($this->haFlags as $flag => $value) {
+          if ($value) {
+              $flags[] = $flag;
+          }
+      }
+      $code.="  ".join(" | ", $flags).", /* handlder flags */\n";
 
-	}
+      $code.="  ".$this->funcName("binlog_func").", /* */\n";
+      $code.="  ".$this->funcName("binlog_log_query").", /* */\n";
+
+      $code.="};\n";
+    }
 }
